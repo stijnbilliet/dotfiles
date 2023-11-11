a7e3bd315f30b8a90eab0fe38c519288449f5026
Stijn Billiet <stijnbilliet@protonmail.com>
2023-11-11 21:11:05 +0000

    Updating the slurp script, possibly will have to convert this to bash script for better posix compliance and easier command substitution

--- .config/sway/scripts/slurp_bind.fish
+++ .config/sway/scripts/slurp_bind.fish
@@ -1,26 +1,23 @@
-#!/usr/bin/env fish
+#!/usr/bin/fish
 
 # Create a named pipe
-set slurpie (mktemp -u)
-mkfifo $slurpie
+set pipe_name (mktemp -u)
+mkfifo "$pipe_name"
 
 # Start slurp in the background and redirect its output to the named pipe
-begin; slurp > $slurpie; end &
+begin; slurp > pipe_name&; end
 
-# Get the PID of the background process
-set pid $last_pid
-
-# Wait for 100 microseconds
+# Wait for 200 microseconds
 sleep 0.1
 
 # Simulate mouse click
 swaymsg seat - cursor press button1
 
 # Wait for the background process to finish
-wait $pid
+wait
 
-# Pass the output of slurp (from the named pipe) to grim
-cat $slurpie
+# Read out pipe/file
+cat pipe_name
 
 # Remove the named pipe
-rm $slurpie
+rm pipe_name
