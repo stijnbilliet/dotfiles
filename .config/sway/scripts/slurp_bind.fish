a0d5e3fd791b25bee4993d9af427d27ed94cc1e3
Stijn Billiet <stijnbilliet@protonmail.com>
2023-11-12 11:24:44 +0000

    Fixed up the slurp bind, and now finished the print screening modes

--- .config/sway/scripts/slurp_bind.fish
+++ .config/sway/scripts/slurp_bind.fish
@@ -1,23 +1,12 @@
 #!/usr/bin/fish
 
-# Create a named pipe
+# Named pipe using random path
 set pipe_name (mktemp -u)
 mkfifo "$pipe_name"
-
 # Start slurp in the background and redirect its output to the named pipe
 begin; slurp > pipe_name&; end
-
-# Wait for 200 microseconds
-sleep 0.1
-
-# Simulate mouse click
+sleep 0.05
 swaymsg seat - cursor press button1
-
-# Wait for the background process to finish
 wait
-
-# Read out pipe/file
 cat pipe_name
-
-# Remove the named pipe
 rm pipe_name
