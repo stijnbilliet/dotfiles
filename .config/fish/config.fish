3cbe757d830875f38a4e290bdbff2b636a933593
Stijn Billiet <stijnbilliet@protonmail.com>
2023-11-13 16:41:25 +0000

    Added darkman root dir to XDG_DATA_DIRS path - not super pleased with this approach, so open to suggestions (i.e. will possibly change this in the future

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,5 @@ set TERM "alacritty"
 set EDITOR "nvim"
 set VISUAL "nvim"
 set -x MANPAGER "nvim -c 'set ft=man' -"
+# Add darkman root dir to XDG_DATA_DIRS path
+set -gx --path XDG_DATA_DIRS ~/.local/share/darkman/ $XDG_DATA_DIRS
