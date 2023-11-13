cd7f4963e51342482b57e1631f420d0fd1b80da2
Stijn Billiet <stijnbilliet@protonmail.com>
2023-11-13 16:37:09 +0000

    Pushing script that touches wallpaper.lnk symlink and updates it with the respective background image for the chosen light/dark mode

---
+++ .local/share/darkman/dark-mode.d/wallpaper-symlink.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/fish
+
+ln -sf ~/.config/wallpapers/dark_cygnus.jpg/ ~/.config/wallpapers/wallpaper.lnk
---
+++ .local/share/darkman/light-mode.d/wallpaper-symlink.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/fish
+
+ln -sf ~/.config/wallpapers/dusk_light.jpg/ ~/.config/wallpapers/wallpaper.lnk
