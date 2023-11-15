ce8ba591c5360eaf5ab22e974e491c3d11a55d37
Stijn Billiet <stijnbilliet@protonmail.com>
2023-11-15 21:27:59 +0000

    Replacing the power menu with a dmenu style one

---
+++ .config/wofi/scripts/powermenu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+entries="\tLock\n\tLogout\n⏾\tSuspend\n \tReboot\n⏻\tShutdown"
+selected=$(echo -e $entries|wofi --show dmenu --width 250 --height 250 --cache-file /dev/null --color ~/.color.d/colors.wofi | awk '{print tolower($2)}')
+
+case $selected in
+  lock)
+    swaylock -f -c 000000;;
+  logout)
+    swaymsg exit;;
+  suspend)
+    exec systemctl suspend;;
+  reboot)
+    exec systemctl reboot;;
+  shutdown)
+    exec systemctl poweroff -i;;
+esac
