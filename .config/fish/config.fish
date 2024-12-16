179a109ddc893b10a491e661129e1d8b1cce0684
stijnbilliet <stijnbilliet@protonmail.com>
2024-12-16 17:16:57 +0000

    Setting up XF86 keyboard shortcuts to use wireplumber instead of pactl; Setting up correct XDG dirs; Kanshi workspacechanges; etc.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,12 @@ set -x EDITOR "nvim"
 set -x VISUAL "nvim"
 set -x MANPAGER 'nvim +Man!'
 
+# Set XDG base directory defaults
+set -x XDG_CONFIG_HOME (set -q XDG_CONFIG_HOME; and echo $XDG_CONFIG_HOME; or echo $HOME/.config)
+set -x XDG_DATA_HOME (set -q XDG_DATA_HOME; and echo $XDG_DATA_HOME; or echo $HOME/.local/share)
+set -x XDG_CACHE_HOME (set -q XDG_CACHE_HOME; and echo $XDG_CACHE_HOME; or echo $HOME/.cache)
+set -x XDG_RUNTIME_DIR (set -q XDG_RUNTIME_DIR; and echo $XDG_RUNTIME_DIR; or echo /run/user/(id -u))
+
 # Wrap sway to ensure correct env vars are set on login
 function startw
     set -x XDG_CURRENT_DESKTOP sway
--- .config/nvim/lua/plugin/lsp.lua
+++ .config/nvim/lua/plugin/lsp.lua
@@ -52,6 +52,9 @@
                 openscad_lsp = {
                     cmd = { "openscad-lsp", "--stdio" },
                     filetypes = { "openscad" }
+                },
+                gdscript = {
+
                 }
             }
 
