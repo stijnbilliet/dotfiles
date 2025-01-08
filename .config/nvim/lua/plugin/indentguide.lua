f262394a1bf8855ef8be46deff4a202871bd4094
stijnbilliet <stijnbilliet@protonmail.com>
2025-01-08 18:59:57 +0000

    Added a line to denote different blocks of indentation

---
+++ .config/nvim/lua/plugin/indentguide.lua
@@ -0,0 +1,10 @@
+-- Set lualine as statusline
+return {
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        ---@module "ibl"
+        ---@type ibl.config
+        opts = {},
+    }
+}
