e59eeee0a16eb3c0927d6b8761f97265b891dfb8
stijnbilliet <stijnbilliet@protonmail.com>
2025-02-28 18:49:41 +0000

    Adding autocomplete for command mode ':'

--- .config/nvim/lua/plugin/autocomplete.lua
+++ .config/nvim/lua/plugin/autocomplete.lua
@@ -1,38 +1,52 @@
 -- Autocompletion
 return {
-  {
-    'hrsh7th/nvim-cmp',
-    event = "InsertEnter",
-    lazy = true,
-    dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
+    {
+        'hrsh7th/nvim-cmp',
+        event = "InsertEnter",
+        lazy = true,
+        dependencies = {
+            -- Snippet Engine & its associated nvim-cmp source
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
 
-      -- Adds LSP/buffer/path completion capabilities
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-nvim-lsp-signature-help',
-	  'hrsh7th/cmp-path',
+            -- Adds LSP/buffer/path completion capabilities
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-nvim-lsp-signature-help',
+            'hrsh7th/cmp-cmdline',
+            'hrsh7th/cmp-path'
+        },
+        config = function()
+            local cmp = require 'cmp'
+            local luasnip = require 'luasnip'
+            luasnip.config.setup {}
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'nvim_lsp_signature_help' },
+                    { name = 'luasnip' },
+                    { name = 'path' },
+                })
+            });
+
+            -- Add autocompletion in command mode, for both paths and cmds
+            cmp.setup.cmdline(':', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = 'path' },
+                    {
+                        name = 'cmdline',
+                        option = {
+                            ignore_cmds = { 'Man', '!' },
+                        },
+                    },
+                }),
+            })
+        end
     },
-    config = function()
-        local cmp = require 'cmp'
-        local luasnip = require 'luasnip'
-        luasnip.config.setup {}
-
-        ---@diagnostic disable-next-line: missing-fields
-        cmp.setup({
-          snippet = {
-            expand = function(args)
-              luasnip.lsp_expand(args.body)
-            end,
-          },
-          sources = cmp.config.sources({
-            { name = 'nvim_lsp' },
-            { name = 'nvim_lsp_signature_help' },
-            { name = 'luasnip' },
-            { name = 'path' }
-          })
-        });
-    end
-  },
 }
