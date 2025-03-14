ffdccae0651b91c9e1e2449e5c0cfd2f25bfcda8
stijnbilliet <stijnbilliet@protonmail.com>
2025-03-14 14:37:26 +0000

    Cleaning up dap config

--- .config/nvim/lua/aether/plugins/debugger.lua
+++ .config/nvim/lua/aether/plugins/debugger.lua
@@ -1,60 +1,51 @@
 return {
-    "mfussenegger/nvim-dap",
-    event='VeryLazy',
-    dependencies = {
-
-        -- fancy UI for the debugger
-        {
-            "rcarriga/nvim-dap-ui",
+    -- dap and friends
+    {
+        "mfussenegger/nvim-dap",
+        event='VeryLazy',
+        dependencies = {
+            "rcarriga/nvim-dap-ui", -- debugger ui
+            "theHamsta/nvim-dap-virtual-text", -- virtual text for the debugger
             "nvim-neotest/nvim-nio",
-            -- stylua: ignore
-            opts = {},
-            config = function(_, opts)
-
-                -- Automatically open dapui when debugging is started
-                local dap = require("dap")
-                local dapui = require("dapui")
-                dapui.setup(opts)
-                dap.listeners.after.event_initialized["dapui_config"] = function()
-                    dapui.open({})
-                end
-                dap.listeners.before.event_terminated["dapui_config"] = function()
-                    dapui.close({})
-                end
-                dap.listeners.before.event_exited["dapui_config"] = function()
-                    dapui.close({})
-                end
-
-            end,
         },
+        config = function()
+            local dap = require("dap")
+            local dapui = require("dapui")
+            local dapvt = require("nvim-dap-virtual-text")
 
-        -- virtual text for the debugger
-        {
-            "theHamsta/nvim-dap-virtual-text",
-            opts = {},
-        },
+            dapui.setup()
+            dapvt.setup()
 
-        -- mason.nvim integration
-        {
-            "jay-babu/mason-nvim-dap.nvim",
-            dependencies = "mason.nvim",
-            cmd = { "DapInstall", "DapUninstall" },
-            opts = {
-                -- Makes a best effort to setup the various debuggers with
-                -- reasonable debug configurations
-                automatic_installation = true,
+            -- Automatically open dapui when debugging is started.
+            dap.listeners.after.event_initialized["dapui_config"] = function()
+                dapui.open({})
+            end
+            dap.listeners.before.event_terminated["dapui_config"] = function()
+                dapui.close({})
+            end
+            dap.listeners.before.event_exited["dapui_config"] = function()
+                dapui.close({})
+            end
+        end,
+    },
+    -- mason.nvim integration
+    {
+        "jay-babu/mason-nvim-dap.nvim",
+        dependencies = "mason.nvim",
+        cmd = { "DapInstall", "DapUninstall" },
+        opts = {
+            -- Makes a best effort to setup the various debuggers with reasonable defaults
+            automatic_installation = true,
 
-                -- You can provide additional configuration to the handlers,
-                -- see mason-nvim-dap README for more information
-                handlers = {},
+            -- You can provide additional configuration to the handlers,
+            -- see mason-nvim-dap README for more information
+            handlers = {},
 
-                -- You'll need to check that you have the required things installed
-                -- online, please don't ask me how to install them :)
-                ensure_installed = {
-                    "codelldb",
-                    "python",
-                    -- Update this to ensure that you have the debuggers for the langs you want
-                },
+            -- Note(stijn): You'll also want to ensure you have the
+            -- relevant adapters installed on your system.
+            ensure_installed = {
+                "codelldb",
+                "python",
             },
         },
     },
