27d819a2adc449705820d8187ee4f19a527a17e8
stijnbilliet <stijnbilliet@protonmail.com>
2025-03-18 14:46:21 +0000

    Adding treesitter function scope to status line

--- .config/nvim/lua/aether/plugins/statusline.lua
+++ .config/nvim/lua/aether/plugins/statusline.lua
@@ -3,13 +3,23 @@ return {
     {
         'nvim-lualine/lualine.nvim',
         event = "VeryLazy",
-        opts = {
-            options = {
-                icons_enabled = false,
-                theme = 'auto',
-                component_separators = '|',
-                section_separators = '',
-            },
-        },
+        config = function()
+            require('lualine').setup {
+                options = {
+                    icons_enabled = true,
+                    theme = 'auto',
+                    component_separators = '|',
+                    section_separators = '',
+                },
+                sections = {
+                    lualine_a = {'mode'},
+                    lualine_b = {'branch', 'diff'},
+                    lualine_c = {'filename'},
+                    lualine_x = {'nvim_treesitter#statusline'},
+                    lualine_y = {'filetype'},
+                    lualine_z = {'progress', 'location'}
+                }
+            }
+        end
     },
 }
