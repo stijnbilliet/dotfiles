f9364bbbb7b86843762728b07e36bfc516ff1001
stijnbilliet <stijnbilliet@protonmail.com>
2026-07-20 20:30:16 +0100

    Adding lsp status to statusline - replaces fidget.nvim plugin dependency

--- .config/nvim/lua/aether/plugins/statusline.lua
+++ .config/nvim/lua/aether/plugins/statusline.lua
@@ -10,12 +10,7 @@ return {
             sections = {
                 lualine_a = {'mode'},
                 lualine_b = {'filename'},
-                lualine_c = {
-                    function()
-                        local clients = vim.lsp.get_clients({ bufnr = 0 })
-                        return #clients > 0 and clients[1].name or ''
-                    end,
-                },
+                lualine_c = {'lsp_status'},
                 lualine_x = {'diagnostics'},
                 lualine_y = {'branch', 'diff'},
                 lualine_z = {'progress', 'location'}
